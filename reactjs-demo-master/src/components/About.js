40ca47de010d30ed96fce66d90b0939de120fe9a
Kishor Ronge <kishorronge1@gmail.com>
2020-03-04 03:05:37 +0530

    Adding sample project

---
+++ reactjs-demo-master/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const About = () => {
+  return (
+    <div className="container">
+      <h1>About</h1>
+      <p>Routes working correctly</p>
+    </div>
+  );
+};
+
+
+export default About;
